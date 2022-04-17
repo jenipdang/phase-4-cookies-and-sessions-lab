0e6d3bd0bbebb03cfc917ec127ee8b2287f5e092
Jeni Dang <jenipdang@yahoo.com>
2022-04-17 16:48:57 -0700

    commit tree c44caa5a3ac945a98409de876df2460aa1f7dad7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,19 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    #set the initial value of 0
+    session[:page_views] ||= 0
+    #increment the value by 1 for every request
+    session[:page_views] += 1
+    
+    #if user has 3 or less views, render the request article data
+    if session[:page_views] <= 3
     article = Article.find(params[:id])
     render json: article
+    else
+      #if more than 3, render the custome message with status code 401, unauthorized
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
